b3a7984bb1e07cecefc9590fb066dd18d53d049f
Jesimiel <95407428+11kainani@users.noreply.github.com>
2024-12-30 11:31:39 +0100

    fix: Customize Record Stack

--- front/src/navigation/stack/RecordStack.tsx
+++ front/src/navigation/stack/RecordStack.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RecordScreen from '../../screens/record/RecordScreen';
 import RecordSettings from '../../screens/record/RecordSettings'
+import { COLORS } from '../../utils';
 
 const Stack = createNativeStackNavigator();
 
 const RecordStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ contentStyle: { flex: 1, backgroundColor: COLORS.backgroundDark }, headerShown: false }}>
       <Stack.Screen name="RecordHome" component={RecordScreen} />
       <Stack.Screen name="Record Settings" component={RecordSettings} />
     </Stack.Navigator>
