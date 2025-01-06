dec3a6db716ee69cac6a0eeea0734411dfb5be84
Jesimiel <95407428+11kainani@users.noreply.github.com>
2025-01-06 14:43:15 +0100

    doc: Subject services

--- front/src/api/services/subjectService.tsx
+++ front/src/api/services/subjectService.tsx
@@ -3,6 +3,19 @@ import apiConfig from '../config/apiConfig';
 
 const subjectEndpoint = "subject";
 
+
+/**
+ * Fetches all subjects from the API.
+ *
+ * This function sends a GET request to the subjects endpoint to retrieve
+ * all available subjects. If the request is successful, it returns the
+ * parsed JSON data. Otherwise, it throws an error.
+ *
+ * @async
+ * @function
+ * @throws {Error} Throws an error if the API response is not ok or if there is a network issue.
+ * @returns {Promise<Object>} A promise that resolves to the data containing all subjects.
+ */
 export const getSubjects = async () => {
     try {
         const url = `${apiConfig.baseURL}/${subjectEndpoint}/all`;
