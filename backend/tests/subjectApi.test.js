bde1a555d4fbbb06871d567d3f9bb5a2d8de4cdd
Jesimiel <95407428+11kainani@users.noreply.github.com>
2025-03-24 09:12:32 +0100

    (back) fix: delete the created data created by tes

--- backend/tests/subjectApi.test.js
+++ backend/tests/subjectApi.test.js
@@ -5,7 +5,7 @@ describe("Subject API Tests", () => {
 
     let createdSubjectId; 
     it("POST /subject/create - should create a subject", async () => {
-        const res = await request(app).post("/subject/create").send({ description: "Test-Subject" }).set('x-api-key', process.env.API_KEY);
+        const res = await request(app).post("/subject/create").send({ description: "TSubject" }).set('x-api-key', process.env.API_KEY);
         createdSubjectId = res.body.Subject.idSubject;
         expect(res.statusCode).toBe(201);
     });
@@ -16,23 +16,26 @@ describe("Subject API Tests", () => {
 
     });
 
-    it("POST /subjects - should fail if no description", async () => {
+    it("POST /subject/create - should fail if no description", async () => {
         const res = await request(app).post("/subject/create").send({}).set('x-api-key', process.env.API_KEY);
         expect(res.statusCode).toBe(400);
     });
 
-    it("GET /subjects - should fetch subjects", async () => {
+    it("GET /subject/create - should fetch subjects", async () => {
         const res = await request(app).get("/subject/all").set('x-api-key', process.env.API_KEY);
         expect(res.statusCode).toBe(200);
     });
 
-    it("POST /subjects - should fail if description is too long", async () => {
+    it("POST /subject/create - should fail if description is too long", async () => {
         const longDescription = "a".repeat(401); // Adjust max length
         const res = await request(app).post("/subject/create").send({ description: longDescription }).set('x-api-key', process.env.API_KEY);
         expect(res.statusCode).toBe(400);
     });
 
-
-
+ 
+    it("DELETE /subjects/delete - should delete the subject that was created by the first test", async () => {
+        const res = await request(app).delete("/subject/delete").query({idSubject : createdSubjectId}).set('x-api-key', process.env.API_KEY);
+        expect(res.statusCode).toBe(200);
+    });
     
 });
