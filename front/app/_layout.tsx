0d1c378bba9e76a9adc75888b4057b6d0c97b88d
Jesimiel <95407428+11kainani@users.noreply.github.com>
2025-04-26 14:24:59 +0200

    (front)  fix: layout icons and size

--- front/app/_layout.tsx
+++ front/app/_layout.tsx
@@ -1,25 +1,25 @@
 import { Tabs } from "expo-router";
-import { Feather, Ionicons } from '@expo/vector-icons';
-import { COLORS } from "../utils";
+import { Entypo, Feather, FontAwesome5, Ionicons } from '@expo/vector-icons';
+import { COLORS, DIMENSIONS } from "../utils";
 
 export default function Layout() {
   return (
     <Tabs screenOptions={{ headerShown: false, 
 
-    tabBarActiveTintColor: COLORS.primaryButton,      // primary blue
-    tabBarInactiveTintColor: COLORS.secondaryBackground,    // secondary accent
+    tabBarActiveTintColor: COLORS.primary,      // primary blue
+    tabBarInactiveTintColor: COLORS.disabled,    // secondary accent
      }}>
       <Tabs.Screen name="home" options={{ title: "Home", 
-         tabBarIcon: ({ color }) => <Feather size={24} name="home" color={color}/>
+         tabBarIcon: ({ color }) => <Feather size={DIMENSIONS.iconSizeLarge} name="home" color={color}/>
         }} />
       <Tabs.Screen name="record" options={{ title: "Record",
-         tabBarIcon: ({ color }) => <Ionicons name="recording" size={24} color={color} />
+         tabBarIcon: ({ color }) => <Ionicons name="recording" size={DIMENSIONS.iconSizeLarge} color={color} />
        }} />
       <Tabs.Screen name="subject" options={{ title: "Subjects",
-         tabBarIcon: ({ color }) => <Feather name="database" size={24} color={color}  />
+         tabBarIcon: ({ color }) => <FontAwesome5 name="database" size={DIMENSIONS.iconSizeLarge} color={color}  />
        }} />
       <Tabs.Screen name="answer" options={{ title: "Answers",
-         tabBarIcon: ({ color }) => <Feather name="message-square" size={24} color={color}  />
+         tabBarIcon: ({ color }) => <Entypo name="message" size={DIMENSIONS.iconSizeLarge} color={color}  />
        }} />
     </Tabs>
   );
